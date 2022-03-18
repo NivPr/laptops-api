e6a6cc95008cd70d2ca78af62068e106a0993edc
NivPr <100204321+NivPr@users.noreply.github.com>
2022-03-18 12:56:50 +0200

    Update mongoConnect.js

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -5,6 +5,6 @@ const {config} = require('../config/config.js');
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(`mongodb+srv://nivp:${config.mongoPass}@cluster0.xlj5r.mongodb.net/node_pro`);
+  await mongoose.connect(`mongodb+srv://${config.mongoUser}:${config.mongoPass}@cluster0.xlj5r.mongodb.net/node_pro`);
   console.log("mongo connect node_pro...")
 }
